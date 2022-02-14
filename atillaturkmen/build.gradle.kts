e32f1a24a39547babea64f83c8c1732267e25a59
Muhammad Faisal Amir <faisalamircs@gmail.com>
2022-02-14 15:29:29 +0700

    update: migrate from gradle to kotlin dsl build.gradle.kts

---
+++ atillaturkmen/build.gradle.kts
@@ -0,0 +1,72 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+}
+
+android {
+
+    compileSdk = ProjectSetting.PROJECT_COMPILE_SDK
+
+    defaultConfig {
+        applicationId = "${ProjectSetting.PROJECT_APP_ID}.${ProjectSetting.atillaturkmen}"
+        minSdk = ProjectSetting.PROJECT_MIN_SDK
+        targetSdk = ProjectSetting.PROJECT_TARGET_SDK
+        versionCode = ProjectSetting.PROJECT_VERSION_CODE
+        versionName = ProjectSetting.PROJECT_VERSION_NAME
+
+        multiDexEnabled = true
+        vectorDrawables.useSupportLibrary = true
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        // Naming APK // AAB
+        setProperty("archivesBaseName", "${ProjectSetting.NAME_APK}(${versionName})")
+
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    packagingOptions {
+        resources {
+            excludes += setOf("META-INF/AL2.0", "META-INF/LGPL2.1")
+        }
+    }
+
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = JavaVersion.VERSION_11.toString()
+        }
+    }
+
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.4.1")
+    implementation("com.google.android.material:material:1.5.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+}
